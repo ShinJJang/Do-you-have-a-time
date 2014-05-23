77b02dfe5ee748ae4662faf5eb020c79eea4f725
Shin JJang <makao.rule@gmail.com>
2014-05-24 02:30:35 +0900

    heroku init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn doyouhavetime.wsgi
--- doyouhavetime/settings.py
+++ doyouhavetime/settings.py
@@ -55,13 +55,18 @@ WSGI_APPLICATION = 'doyouhavetime.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
